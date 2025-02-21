e223150c65a83aff756954815a8ee1cfd835110d
Turbojax07 <turbojax07@gmail.com>
2025-02-21 00:31:25 -0500

    Adding the elevator subsystem

---
+++ src/main/java/frc/robot/Subsystems/Elevator/Elevator.java
@@ -0,0 +1,102 @@
+package frc.robot.Subsystems.Elevator;
+
+import static frc.robot.Subsystems.Elevator.ElevatorConstants.*;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.trajectory.ExponentialProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
+public class Elevator extends SubsystemBase {
+    private final ElevatorIO io;
+    private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+
+    private final ExponentialProfile profile = new ExponentialProfile(ExponentialProfile.Constraints.fromCharacteristics(maxProfileVoltage - s - g, v, a));
+    private ExponentialProfile.State profileState = new ExponentialProfile.State(0.0, 0.0);
+    private ExponentialProfile.State futureProfileState = new ExponentialProfile.State(0.0, 0.0);
+
+    private ElevatorFeedforward feedforward = new ElevatorFeedforward(s, g, v);
+
+    private double position;
+
+    private final SysIdRoutine routine;
+
+    public Elevator(ElevatorIO io) {
+        this.io = io;
+
+        periodic();
+
+        profileState.position = inputs.position;
+
+        routine = new SysIdRoutine(
+            new SysIdRoutine.Config(
+                Velocity.ofRelativeUnits(sysIdRampUp, Units.Volts.per(Units.Seconds)), 
+                Voltage.ofRelativeUnits(sysIdStep, Units.Volts), 
+                Time.ofRelativeUnits(sysIdTimeout, Units.Seconds)
+            ), 
+            new SysIdRoutine.Mechanism(
+                voltage -> io.setPosition(0, voltage.magnitude()),
+                log -> {
+                    log.motor("elevator")
+                        .voltage(Voltage.ofRelativeUnits(inputs.voltages[0], Units.Volts))
+                        .linearPosition(Distance.ofRelativeUnits(inputs.position, Units.Meters))
+                        .linearVelocity(LinearVelocity.ofRelativeUnits(inputs.velocity, Units.MetersPerSecond));
+                },
+                this
+            )
+        );
+    }
+
+    @Override
+    public void periodic() {
+        futureProfileState = profile.calculate(0.02, profileState, new ExponentialProfile.State(position, 0.0));
+        Logger.recordOutput("Elevator/Position/Setpoint", profileState.position);
+        Logger.recordOutput("Elevator/Velocity/Setpoint", profileState.velocity);
+        double feedforwardValue = feedforward.calculateWithVelocities(profileState.velocity, futureProfileState.velocity);
+        Logger.recordOutput("Elevator/Feedforward", feedforwardValue);
+        io.setPosition(futureProfileState.position, feedforwardValue);
+        profileState = futureProfileState;
+
+        io.updateInputs(inputs);
+        Logger.processInputs("Elevator", inputs);
+    }
+
+    @AutoLogOutput(key="Elevator/Position/Measured")
+    public double getPosition() {
+        return inputs.position;
+    }
+
+    public void setPosition(double position) {
+        Logger.recordOutput("Elevator/Position/Ideal", position);
+        this.position = position;
+    }
+
+    @AutoLogOutput(key="Elevator/Velocity/Measured")
+    public double getVelocity() {
+        return inputs.velocity;
+    }
+
+    public void reset() {
+        io.reset();
+    }
+
+    public Command sysIdRoutine() {
+        return Commands.sequence(
+            routine.quasistatic(SysIdRoutine.Direction.kForward).until(() -> inputs.position > sysIdMaxPosition),
+            routine.quasistatic(SysIdRoutine.Direction.kReverse).until(() -> inputs.position < sysIdMinPosition),
+            routine.dynamic(SysIdRoutine.Direction.kForward).until(() -> inputs.position > sysIdMaxPosition),
+            routine.dynamic(SysIdRoutine.Direction.kReverse).until(() -> inputs.position < sysIdMinPosition)
+        );
+    }
+}
---
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorIOReal.java
@@ -0,0 +1,65 @@
+package frc.robot.Subsystems.Elevator;
+
+import static frc.robot.Subsystems.Elevator.ElevatorConstants.*;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class ElevatorIOReal implements ElevatorIO {
+    private final SparkMax leftMotor;
+    private final SparkMax rightMotor;
+
+    private final RelativeEncoder encoder;
+    private final SparkClosedLoopController feedback;
+
+    public ElevatorIOReal(int leftId, int rightId) {
+        leftMotor = new SparkMax(leftId, MotorType.kBrushless);
+        rightMotor = new SparkMax(rightId, MotorType.kBrushless);
+
+        SparkMaxConfig config = new SparkMaxConfig();
+        config
+            .inverted(false)
+            .smartCurrentLimit(60)
+            .idleMode(IdleMode.kCoast);
+        config.encoder
+            .positionConversionFactor(positionConversionFactor)
+            .velocityConversionFactor(velocityConversionFactor);
+        config.closedLoop
+            .p(p)
+            .i(i)
+            .d(d);
+        leftMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        config
+            .follow(leftMotor, true);
+        rightMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        encoder = leftMotor.getEncoder();
+        feedback = leftMotor.getClosedLoopController();
+    }
+
+    @Override
+    public void updateInputs(ElevatorIOInputs inputs) {
+        inputs.position = encoder.getPosition();
+        inputs.velocity = encoder.getVelocity();
+        inputs.voltages = new double[] {leftMotor.getAppliedOutput() * leftMotor.getBusVoltage() , rightMotor.getAppliedOutput() * rightMotor.getBusVoltage()};
+        inputs.currents = new double[] {leftMotor.getOutputCurrent(), rightMotor.getOutputCurrent()};
+    }
+
+    @Override
+    public void setPosition(double position, double ffVoltage) {
+        feedback.setReference(position, ControlType.kPosition, ClosedLoopSlot.kSlot0, ffVoltage);
+    }
+
+    @Override
+    public void reset() {
+        encoder.setPosition(0.0);
+    }
+}

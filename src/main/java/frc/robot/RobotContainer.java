4cc1eca82bd9d7026fc3b1fe294d255defbfa079
turbojax07 <turbojax07@gmail.com>
2025-03-03 23:11:20 -0500

    Removing getInstance in accordance with new rules

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import choreo.auto.AutoFactory;
@@ -21,26 +17,26 @@ public class RobotContainer {
     private CommandXboxController driverController = new CommandXboxController(0);
     private CommandXboxController operatorController = new CommandXboxController(1);
 
-    public Carriage carriage;
-    public Drivetrain drivetrain;
-    public Elevator elevator;
-    public Gyro gyro;
+    private Carriage carriage;
+    private Drivetrain drivetrain;
+    private Elevator elevator;
+    private Gyro gyro;
 
     public RobotContainer() {
         // Initializing subsystems
         if (Robot.isReal()) {
             gyro = new Gyro(new GyroIOPigeon2(RobotMap.GYRO_Pigeon2Id));
-            drivetrain = new Drivetrain(new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
+            drivetrain = new Drivetrain(gyro, new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
             carriage = new Carriage(new CarriageIOSparkMax(RobotMap.CARRIAGE_MOTOR_ID, RobotMap.CARRIAGE_CANANDCOLOR_ID));
             elevator = new Elevator(new ElevatorIOReal(RobotMap.ELEV_LeftId, RobotMap.ELEV_RightId));
         } else {
-            drivetrain = new Drivetrain(new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
+            drivetrain = new Drivetrain(gyro, new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
             carriage = new Carriage(new CarriageIOSim());
             elevator = new Elevator(new ElevatorIOSim());
         }
 
         // Assigning default commands
-        drivetrain.setDefaultCommand(new SwerveDrive(driverController::getLeftX, driverController::getLeftY, driverController::getRightX));
+        drivetrain.setDefaultCommand(new SwerveDrive(drivetrain, driverController::getLeftX, driverController::getLeftY, driverController::getRightX));
 
         // Prepping Choreo
         // AutoFactory autoFactory = new AutoFactory(drivetrain::getPose, drivetrain::resetPose, drivetrain::followTrajectory, true, drivetrain);
@@ -52,8 +48,8 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        operatorController.a().whileTrue(new RunCarriage(1));
-        operatorController.b().whileTrue(new RunCarriage(-1));
+        operatorController.a().whileTrue(new RunCarriage(carriage, 1));
+        operatorController.b().whileTrue(new RunCarriage(carriage, -1));
 
         // Move up/down with pov up/down
         //driverController.povUp().onTrue(new MoveUp(elevator));
@@ -66,7 +62,7 @@ public class RobotContainer {
         operatorController.povDown().onTrue(new SetPosition(ElevatorPosition.STOW, elevator));
 
         // driverController.a().whileTrue(new PathfindToPose());
-        driverController.x().toggleOnTrue(new XStates());
+        driverController.x().toggleOnTrue(new XStates(drivetrain));
     }
 
     public Command getAutonomousCommand() {

997249d541c7324b70f6a1d1ce9851acdaaff09e
turbojax07 <turbojax07@gmail.com>
2025-03-04 01:17:27 -0500

    Formatted

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
@@ -19,10 +15,10 @@ public class Robot extends LoggedRobot {
     private RobotContainer robotContainer;
     private Command autonomousCommand;
     private Command teleopCommand;
-    
+
     public Robot() {
         robotContainer = new RobotContainer();
-        
+
         Logger.addDataReceiver(new NT4Publisher());
 
         if (isReal()) {

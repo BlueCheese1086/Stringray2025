9dc82f95cf47d01bcd37bdacac6aa9669878b45c
turbojax07 <turbojax07@gmail.com>
2025-03-03 23:37:52 -0500

    Fixing errors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import choreo.auto.AutoFactory;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.RobotMap;
@@ -29,12 +30,12 @@ public class RobotContainer {
         if (Robot.isReal()) {
             gyro = new Gyro(new GyroIOPigeon2(RobotMap.GYRO_Pigeon2Id));
             vision = new Vision(new CameraIOReal("", new Transform3d()), new CameraIOReal("", new Transform3d()));
-            drivetrain = new Drivetrain(gyro, vision, new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
+            drivetrain = new Drivetrain(new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
             carriage = new Carriage(new CarriageIOSparkMax(RobotMap.CARRIAGE_MOTOR_ID, RobotMap.CARRIAGE_CANANDCOLOR_ID));
             elevator = new Elevator(new ElevatorIOReal(RobotMap.ELEV_LeftId, RobotMap.ELEV_RightId));
         } else {
             vision = new Vision(new CameraIOSim("", new Transform3d()), new CameraIOSim("", new Transform3d()));
-            drivetrain = new Drivetrain(gyro, vision, new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
+            drivetrain = new Drivetrain(new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
             carriage = new Carriage(new CarriageIOSim());
             elevator = new Elevator(new ElevatorIOSim());
         }
@@ -52,8 +53,8 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        operatorController.a().whileTrue(new RunCarriage(1));
-        operatorController.b().whileTrue(new RunCarriage(-1));
+        operatorController.a().whileTrue(new RunCarriage(carriage, 1));
+        operatorController.b().whileTrue(new RunCarriage(carriage, -1));
 
         // Move up/down with pov up/down
         //driverController.povUp().onTrue(new MoveUp(elevator));

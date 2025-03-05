a97f0107f6f325d84576c2a13335728f807b0f73
turbojax07 <turbojax07@gmail.com>
2025-03-04 20:16:54 -0500

    Updating controls and Vision constructors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 import choreo.auto.AutoFactory;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.RobotMap;
@@ -10,8 +9,7 @@ import frc.robot.subsystems.carriage.commands.*;
 import frc.robot.subsystems.drivetrain.*;
 import frc.robot.subsystems.drivetrain.commands.*;
 import frc.robot.subsystems.elevator.*;
-import frc.robot.subsystems.elevator.Elevator.ElevatorPosition;
-import frc.robot.subsystems.elevator.commands.SetPosition;
+import frc.robot.subsystems.elevator.commands.*;
 import frc.robot.subsystems.gyro.*;
 import frc.robot.subsystems.vision.*;
 
@@ -29,12 +27,16 @@ public class RobotContainer {
         // Initializing subsystems
         if (Robot.isReal()) {
             gyro = new Gyro(new GyroIOPigeon2(RobotMap.GYRO_Pigeon2Id));
-            vision = new Vision(new CameraIOReal("", new Transform3d()), new CameraIOReal("", new Transform3d()));
+            vision = new Vision(
+                new CameraIOSim(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
+                new CameraIOSim(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
             drivetrain = new Drivetrain(gyro, vision, new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
             carriage = new Carriage(new CarriageIOSparkMax(RobotMap.CARRIAGE_MOTOR_ID, RobotMap.CARRIAGE_CANANDCOLOR_ID));
             elevator = new Elevator(new ElevatorIOReal(RobotMap.ELEV_LeftId, RobotMap.ELEV_RightId));
         } else {
-            vision = new Vision(new CameraIOSim("", new Transform3d()), new CameraIOSim("", new Transform3d()));
+            vision = new Vision(
+                new CameraIOReal(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
+                new CameraIOReal(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
             drivetrain = new Drivetrain(gyro, vision, new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
             carriage = new Carriage(new CarriageIOSim());
             elevator = new Elevator(new ElevatorIOSim());
@@ -57,16 +59,18 @@ public class RobotContainer {
         operatorController.b().whileTrue(new RunCarriage(carriage, -1));
 
         // Move up/down with pov up/down
-        //driverController.povUp().onTrue(new MoveUp(elevator));
-        //driverController.povDown().onTrue(new MoveDown(elevator));
+        driverController.povUp().onTrue(new MoveUp(elevator));
+        driverController.povDown().onTrue(new MoveDown(elevator));
+        driverController.povLeft().onTrue(new SetPosition(Elevator.ElevatorPosition.STOW, elevator));
 
         // Button per state
-        operatorController.povUp().onTrue(new SetPosition(ElevatorPosition.L4, elevator));
-        operatorController.povLeft().onTrue(new SetPosition(ElevatorPosition.L3, elevator));
-        operatorController.povRight().onTrue(new SetPosition(ElevatorPosition.L2, elevator));
-        operatorController.povDown().onTrue(new SetPosition(ElevatorPosition.STOW, elevator));
+        // operatorController.povUp().onTrue(new SetPosition(Elevator.ElevatorPosition.L4, elevator));
+        // operatorController.povLeft().onTrue(new SetPosition(Elevator.ElevatorPosition.L3, elevator));
+        // operatorController.povRight().onTrue(new SetPosition(Elevator.ElevatorPosition.L2, elevator));
+        // operatorController.povDown().onTrue(new SetPosition(Elevator.ElevatorPosition.INTAKE, elevator));
 
         driverController.x().toggleOnTrue(new XStates(drivetrain));
+        driverController.y().onTrue(new RecordPose(drivetrain));
 
         driverController.rightStick().toggleOnTrue(new PathfindToPose(drivetrain, drivetrain.getClosestReefPoint()));
     }

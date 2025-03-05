9d565608449f7d7716818a303504b7c4f1e4f61a
turbojax07 <turbojax07@gmail.com>
2025-03-04 20:28:12 -0500

    Making a simple command that logs the current robot pose when you press a button.

---
+++ src/main/java/frc/robot/RecordPose.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+public class RecordPose extends Command {
+    int x = 0;
+    Drivetrain drivetrain;
+
+    public RecordPose(Drivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public void execute() {
+        x += 1;
+        Logger.recordOutput("/RecordedPoses/" + x, drivetrain.getPose());
+    }
+}

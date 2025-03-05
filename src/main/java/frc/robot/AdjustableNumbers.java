1959b519d7fbef5a6446658f4af745ff24755d18
turbojax07 <turbojax07@gmail.com>
2025-03-04 20:27:47 -0500

    Adding comments, and fixing issues with the hasChanged array

--- src/main/java/frc/robot/AdjustableNumbers.java
+++ src/main/java/frc/robot/AdjustableNumbers.java
@@ -8,39 +8,86 @@ public class AdjustableNumbers {
     private static HashMap<String,Double> loggedValues = new HashMap<String,Double>();
     private static HashMap<String,Boolean> hasChanged = new HashMap<String,Boolean>();
 
-    public static void register(String key) {
-        register(key, key, 0);
+    /**
+     * Adds a value to the logger.
+     * The default value is 0.
+     * 
+     * @return Returns false if the value already exists.
+     */
+    public static boolean register(String key) {
+        return register(key, key, 0);
     }
 
-    public static void register(String shortName, String ntKey) {
-        register(shortName, ntKey, 0);
+    /**
+     * Adds a value to the logged values.
+     * The default value is 0.
+     * 
+     * @param shortName The short name to get the value with.
+     * @param ntKey The NetworkTables key for logging.
+     * 
+     * @return Returns false if the value already exists.
+     */
+    public static boolean register(String shortName, String ntKey) {
+        return register(shortName, ntKey, 0);
     }
 
-    public static void register(String shortName, String ntKey, double defaultValue) {
-        if (loggedNetworkNumbers.containsKey(shortName)) return;
+    /**
+     * Adds a value to the logged values.
+     * 
+     * @param shortName The short name to get the value with.
+     * @param ntKey The NetworkTables key for logging.
+     * @param defaultValue The default value to retrieve from the AKit Logger.
+     * 
+     * @return Returns false if the value already exists.
+     */
+    public static boolean register(String shortName, String ntKey, double defaultValue) {
+        if (loggedNetworkNumbers.containsKey(shortName)) return false;
 
         loggedNetworkNumbers.put(shortName, new LoggedNetworkNumber(ntKey, defaultValue));
         loggedValues.put(shortName, defaultValue);
         hasChanged.put(shortName, false);
+
+        return true;
     }
 
+    /**
+     * Gets a value from the logger and marks it as read.
+     * 
+     * @param shortName The first parameter from the register() function.
+     */
     public static double getValue(String shortName) {
+        hasChanged.put(shortName, false);
+
         return loggedValues.get(shortName);
     }
 
+    /**
+     * Gets whether or not the value has changed.
+     * 
+     * @param shortName The first parameter from the register() function.
+     */
     public static boolean hasChanged(String shortName) {
         return hasChanged.get(shortName);
     }
 
+    /**
+     * Updates the values with their current value in NetworkTables and updates the corresponding hasChanged value accordingly.
+     */
     public static void updateValues() {
         for (String shortName : loggedNetworkNumbers.keySet()) {
-            if (loggedNetworkNumbers.get(shortName).get() != loggedValues.get(shortName)) {
-                loggedValues.put(shortName, loggedNetworkNumbers.get(shortName).get());
+            double loggedValue = loggedNetworkNumbers.get(shortName).get();
+            if (loggedValue != loggedValues.get(shortName)) {
+                loggedValues.put(shortName, loggedValue);
                 hasChanged.put(shortName, true);
             }
         }
     }
 
+    /**
+     * Removes a value from the logger.
+     * 
+     * @param shortName The value to remove.
+     */
     public static void remove(String shortName) {
         loggedNetworkNumbers.remove(shortName);
         loggedValues.remove(shortName);

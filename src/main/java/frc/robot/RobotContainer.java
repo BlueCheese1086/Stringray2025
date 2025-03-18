72a9d94fbc5ad34842ff1caefc4609556c76f969
turbojax07 <turbojax07@gmail.com>
2025-03-18 18:27:32 -0400

    Isolating Elevator and binding more buttons for testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Volts;
+
+import org.littletonrobotics.junction.Logger;
+
 import choreo.auto.AutoFactory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -27,23 +32,23 @@ public class RobotContainer {
         // Initializing subsystems
         if (Robot.isReal()) {
             gyro = new Gyro(new GyroIOPigeon2(RobotMap.GYRO_Pigeon2Id));
-            vision = new Vision(
-                new CameraIOSim(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
-                new CameraIOSim(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
-            drivetrain = new Drivetrain(gyro, vision, new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
-            carriage = new Carriage(new CarriageIOSparkMax(RobotMap.CARRIAGE_MOTOR_ID, RobotMap.CARRIAGE_CANANDCOLOR_ID));
+            // vision = new Vision(
+            //     new CameraIOSim(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
+            //     new CameraIOSim(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
+            // drivetrain = new Drivetrain(gyro, vision, new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
+            // carriage = new Carriage(new CarriageIOSparkMax(RobotMap.CARRIAGE_MotorId, RobotMap.CARRIAGE_CandandColorId));
             elevator = new Elevator(new ElevatorIOReal(RobotMap.ELEV_LeftId, RobotMap.ELEV_RightId));
         } else {
-            vision = new Vision(
-                new CameraIOReal(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
-                new CameraIOReal(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
-            drivetrain = new Drivetrain(gyro, vision, new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
-            carriage = new Carriage(new CarriageIOSim());
+            // vision = new Vision(
+            //     new CameraIOReal(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
+            //     new CameraIOReal(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
+            // drivetrain = new Drivetrain(gyro, vision, new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
+            // carriage = new Carriage(new CarriageIOSim());
             elevator = new Elevator(new ElevatorIOSim());
         }
 
         // Assigning default commands
-        drivetrain.setDefaultCommand(new SwerveDrive(drivetrain, driverController::getLeftX, driverController::getLeftY, driverController::getRightX));
+        // drivetrain.setDefaultCommand(new SwerveDrive(drivetrain, driverController::getLeftX, driverController::getLeftY, driverController::getRightX));
 
         // Prepping Choreo
         // AutoFactory autoFactory = new AutoFactory(drivetrain::getPose, drivetrain::resetPose, drivetrain::followTrajectory, true, drivetrain);
@@ -55,13 +60,20 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        operatorController.a().whileTrue(new RunCarriage(carriage, 1));
-        operatorController.b().whileTrue(new RunCarriage(carriage, -1));
+        // driverController.a().whileTrue(new RunCarriage(carriage, 1));
+        // driverController.b().whileTrue(new RunCarriage(carriage, -1));
 
         // Move up/down with pov up/down
         driverController.povUp().onTrue(new MoveUp(elevator));
         driverController.povDown().onTrue(new MoveDown(elevator));
-        driverController.povLeft().onTrue(new SetPosition(Elevator.ElevatorPosition.STOW, elevator));
+        driverController.a().onTrue(new SetPosition(elevator, Elevator.ElevatorPosition.L1));
+        driverController.b().onTrue(new SetPosition(elevator, Elevator.ElevatorPosition.L2));
+        driverController.x().onTrue(new SetPosition(elevator, Elevator.ElevatorPosition.L3));
+        driverController.y().onTrue(new SetPosition(elevator, Elevator.ElevatorPosition.L4));
+        driverController.povLeft().onTrue(new SetPosition(elevator, Elevator.ElevatorPosition.STOW));
+        driverController.povRight().toggleOnTrue(new ZeroElevator(elevator, Volts.of(-4), Amps.of(18)));
+
+        // driverController.a().onTrue();
 
         // Button per state
         // operatorController.povUp().onTrue(new SetPosition(Elevator.ElevatorPosition.L4, elevator));
@@ -69,10 +81,14 @@ public class RobotContainer {
         // operatorController.povRight().onTrue(new SetPosition(Elevator.ElevatorPosition.L2, elevator));
         // operatorController.povDown().onTrue(new SetPosition(Elevator.ElevatorPosition.INTAKE, elevator));
 
-        driverController.x().toggleOnTrue(new XStates(drivetrain));
-        driverController.y().onTrue(new RecordPose(drivetrain));
+        // driverController.x().toggleOnTrue(new XStates(drivetrain));
+        // driverController.y().onTrue(new RecordPose(drivetrain));
 
-        driverController.rightStick().toggleOnTrue(new PathfindToPose(drivetrain, drivetrain.getClosestReefPoint()));
+        // driverController.rightStick().toggleOnTrue(new PathfindToPose(drivetrain, drivetrain.getClosestReefPoint()));
+    }
+
+    public void periodic() {
+        // Logger.recordOutput("/PlaceToGo", drivetrain.getClosestReefPoint());
     }
 
     public Command getAutonomousCommand() {

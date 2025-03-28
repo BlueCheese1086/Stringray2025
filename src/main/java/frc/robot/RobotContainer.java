f6db30eb2a2791f5b71736d3fa5a8489c4660c3e
Blasty25 <sohamshah210@gmail.com>
2025-03-28 19:33:03 -0400

    test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,49 +37,58 @@ public class RobotContainer {
         if (Robot.isReal()) {
             gyro = new Gyro(new GyroIOPigeon2(Constants.RobotMap.GYRO_Pigeon2Id));
             vision = new Vision(
-                new CameraIOSim(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
-                new CameraIOSim(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
-            drivetrain = new Drivetrain(gyro, vision, new ModuleIOTalonFX(0), new ModuleIOTalonFX(1), new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
-            carriage = new Carriage(new CarriageIOReal(Constants.RobotMap.CARRIAGE_CoralId, Constants.RobotMap.CARRIAGE_AlgaeId, Constants.RobotMap.CARRIAGE_TrackId, Constants.RobotMap.CARRIAGE_CandandColorId));
-            elevator = new Elevator(new ElevatorIOReal(Constants.RobotMap.ELEV_LeftId, Constants.RobotMap.ELEV_RightId));
+                    new CameraIOSim(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
+                    new CameraIOSim(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
+            drivetrain = new Drivetrain(gyro, vision, new ModuleIOTalonFX(0), new ModuleIOTalonFX(1),
+                    new ModuleIOTalonFX(2), new ModuleIOTalonFX(3));
+            carriage = new Carriage(
+                    new CarriageIOReal(Constants.RobotMap.CARRIAGE_CoralId, Constants.RobotMap.CARRIAGE_AlgaeId,
+                            Constants.RobotMap.CARRIAGE_TrackId, Constants.RobotMap.CARRIAGE_CandandColorId));
+            elevator = new Elevator(
+                    new ElevatorIOReal(Constants.RobotMap.ELEV_LeftId, Constants.RobotMap.ELEV_RightId));
         } else {
             vision = new Vision(
-                new CameraIOReal(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
-                new CameraIOReal(Constants.VisionConstants.rCameraName, Constants.VisionConstants.rCameraTransform));
-            drivetrain = new Drivetrain(gyro, vision, new ModuleIOSim(0), new ModuleIOSim(1), new ModuleIOSim(2), new ModuleIOSim(3));
+                    new CameraIOReal(Constants.VisionConstants.lCameraName, Constants.VisionConstants.lCameraTransform),
+                    new CameraIOReal(Constants.VisionConstants.rCameraName,
+                            Constants.VisionConstants.rCameraTransform));
+            drivetrain = new Drivetrain(gyro, vision, new ModuleIOSim(0), new ModuleIOSim(1),
+                    new ModuleIOSim(2), new ModuleIOSim(3));
             carriage = new Carriage(new CarriageIOSim());
             elevator = new Elevator(new ElevatorIOSim());
         }
 
-        // Anti-Tip command (Cancels if the B button is pressed)
+        // Anti-Tip command (Cancels if the A button is pressed)
         new AntiTip(drivetrain, elevator, gyro, () -> driverController.getHID().getAButton()).schedule();
 
         // Creating the drive command
         // Creating it here so that I can pass in an adjustable scalar to limit speeds.
-        driveCommand = new SwerveDrive(drivetrain, driverController::getLeftX, driverController::getLeftY, driverController::getRightX, driverController.getHID()::getLeftStickButtonPressed);
+        driveCommand = new SwerveDrive(drivetrain, driverController::getLeftX, driverController::getLeftY,
+                driverController::getRightX, driverController.getHID()::getLeftStickButtonPressed);
 
         // Assigning default commands
         drivetrain.setDefaultCommand(driveCommand);
 
         // Creating the pathfinding command
-        // It has an override condition that causes it to stop when the left joystick gets any input.
+        // It has an override condition that causes it to stop when the left joystick
+        // gets any input.
         // It is defined this way so that you can change the pose it pathfinds to.
         pathFindingLeft = new PathFindToLeft(drivetrain, () -> {
             return Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getLeftX).get(), 0.1)) > 0 ||
-                   Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getLeftY).get(), 0.1)) > 0 ||
-                   Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getRightX).get(), 0.1)) > 0 ||
-                   Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getRightY).get(), 0.1)) > 0;
+                    Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getLeftY).get(), 0.1)) > 0 ||
+                    Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getRightX).get(), 0.1)) > 0 ||
+                    Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getRightY).get(), 0.1)) > 0;
         });
 
         pathFindingRight = new PathFindToRight(drivetrain, () -> {
             return Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getLeftX).get(), 0.1)) > 0 ||
-                   Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getLeftY).get(), 0.1)) > 0 ||
-                   Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getRightX).get(), 0.1)) > 0 ||
-                   Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getRightY).get(), 0.1)) > 0;
+                    Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getLeftY).get(), 0.1)) > 0 ||
+                    Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getRightX).get(), 0.1)) > 0 ||
+                    Math.abs(MathUtil.applyDeadband(((Supplier<Double>) driverController::getRightY).get(), 0.1)) > 0;
         });
 
         // Prepping Choreo
-        // AutoFactory autoFactory = new AutoFactory(drivetrain::getPose, drivetrain::resetPose, drivetrain::followTrajectory, true, drivetrain);
+        // AutoFactory autoFactory = new AutoFactory(drivetrain::getPose,
+        // drivetrain::resetPose, drivetrain::followTrajectory, true, drivetrain);
         // autoFactory.trajectoryCmd("My Trajectory");
         // autoFactory.newRoutine("My Auto").cmd();
 
@@ -89,32 +98,28 @@ public class RobotContainer {
 
     private void configureBindings() {
 
-        driverController.b().onTrue(Commands.runOnce(()->{gyro.reset();}, gyro));
+        // Driver Controls
+        driverController.y().onTrue(new RecordPose(drivetrain));
+        driverController.b().onTrue(Commands.runOnce(() -> {gyro.reset();}, gyro));
         driverController.x().onTrue(new XStates(drivetrain));
         driverController.leftBumper().whileTrue(Commands.runOnce(() -> driveCommand.setScalar(Constants.PrecisionScalar)));
         driverController.leftBumper().onFalse(Commands.runOnce(() -> driveCommand.setScalar(1)));
 
         driverController.rightBumper().whileTrue(Commands.runOnce(() -> driveCommand.setScalar(Constants.PrecisionScalar)));
         driverController.rightBumper().onFalse(Commands.runOnce(() -> driveCommand.setScalar(1)));
-    
+
         driverController.back().onTrue(pathFindingLeft);
         driverController.start().onTrue(pathFindingRight);
-        
-        driverController.leftTrigger(0.2).whileTrue(new RunCoralRoller(carriage, ()-> -1.0));
-        driverController.leftTrigger(0.2).whileTrue(new RunAlgaeRoller(carriage, ()-> -1.0));
 
-        driverController.rightTrigger(0.2).whileTrue(new RunCoralRoller(carriage, ()-> 1.0));
-        driverController.rightTrigger(0.2).whileTrue(new RunAlgaeRoller(carriage, ()-> 1.0));
+        driverController.leftTrigger(0.2).whileTrue(new RunCoralRoller(carriage, () -> -1.0));
+        driverController.leftTrigger(0.2).whileTrue(new RunAlgaeRoller(carriage, () -> -1.0));
 
-        // Button per state
-        // operatorController.povUp().onTrue(new SetPosition(Elevator.ElevatorPosition.L4, elevator));
-        // operatorController.povLeft().onTrue(new SetPosition(Elevator.ElevatorPosition.L3, elevator));
-        // operatorController.povRight().onTrue(new SetPosition(Elevator.ElevatorPosition.L2, elevator));
-        // operatorController.povDown().onTrue(new SetPosition(Elevator.ElevatorPosition.INTAKE, elevator));
+        driverController.rightTrigger(0.2).whileTrue(new RunCoralRoller(carriage, () -> 1.0));
+        driverController.rightTrigger(0.2).whileTrue(new RunAlgaeRoller(carriage, () -> 1.0));
 
-        // driverController.x().toggleOnTrue(new XStates(drivetrain));
-        // driverController.y().onTrue(new RecordPose(drivetrain));
-
+        // Operator Buttons
+        operatorController.start().onTrue(elevator.resetEncoder());
+        operatorController.back().onTrue(elevator.resetEncoder());
     }
 
     public void periodic() {

9d17a2fc1828f83c6b34589affd2bb09327023b5
turbojax07 <turbojax07@gmail.com>
2025-04-04 18:09:43 -0400

    Making anti-tip use suppliers to allow for more compatibility with other programs.

--- src/main/java/frc/robot/util/AntiTip.java
+++ src/main/java/frc/robot/util/AntiTip.java
@@ -1,32 +1,38 @@
 package frc.robot.util;
 
+import static edu.wpi.first.units.Units.Inches;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
-import frc.robot.subsystems.elevator.Elevator;
-import frc.robot.subsystems.elevator.ElevatorConstants.ElevatorPositions;
-import frc.robot.subsystems.gyro.Gyro;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class AntiTip extends Command {
-    private Elevator elevator;
-    private Gyro gyro;
+    private Consumer<Distance> setHeight;
+    private Supplier<Angle> pitchSupplier;
+    private Supplier<Angle> rollSupplier;
 
     /**
      * Creates a new {@link AntiTip} command.
-     * It checks the roll and pitch of the gyroscope and slams the elevator down if it is above some threshold.
+     * It checks the roll and pitch of the robot and slams the elevator down if it is above some threshold.
      * 
-     * @param elevator The {@link Elevator} to control.
-     * @param gyro The {@link Gyro} to read.
+     * @param setHeight A function that sets the height of the robot's elevator.
+     * @param pitchSupplier A function that gets the pitch of the robot.
+     * @param rollSupplier A function that gets the roll of the robot.
      */
-    public AntiTip(Elevator elevator, Gyro gyro) {
-        this.elevator = elevator;
-        this.gyro = gyro;
+    public AntiTip(Consumer<Distance> setHeight, Supplier<Angle> pitchSupplier, Supplier<Angle> rollSupplier) {
+        this.setHeight = setHeight;
+        this.pitchSupplier = pitchSupplier;
+        this.rollSupplier = rollSupplier;
     }
 
     /** Called every time the scheduler runs while the command is scheduled. */
     @Override
     public void execute() {
-        if (gyro.getPitch().gte(Constants.TipThreshold) || gyro.getRoll().gte(Constants.TipThreshold)) {
-            elevator.setPosition(ElevatorPositions.STOW);
+        if (pitchSupplier.get().gte(Constants.TipThreshold) || rollSupplier.get().gte(Constants.TipThreshold)) {
+            setHeight.accept(Inches.zero());
         }
     }
 }

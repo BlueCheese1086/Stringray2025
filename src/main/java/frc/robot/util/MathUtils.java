3f8394efac253383bfe383f81535a3bb07292654
turbojax07 <turbojax07@gmail.com>
2025-04-05 14:45:05 -0400

    Adding a function that takes either Pose2ds or Translation2ds and finds the mean pose inbetween each one.

--- src/main/java/frc/robot/util/MathUtils.java
+++ src/main/java/frc/robot/util/MathUtils.java
@@ -1,5 +1,8 @@
 package frc.robot.util;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
 public class MathUtils {
     /**
      * Applies a deadband and offsets the output by the deadband.
@@ -40,5 +43,45 @@ public class MathUtils {
      */
     public static boolean withinDeadband(double value, double deadband) {
         return Math.abs(value) < Math.abs(deadband);
+    }
+
+    /**
+     * Finds a {@link Translation2d} between all of the parameters.
+     * @param translations The translations to analyze
+     */
+    public Translation2d getMean(Translation2d... translations) {
+        if (translations.length == 0) return new Translation2d();
+        if (translations.length == 1) return translations[0];
+
+        Translation2d interpolated = translations[0].interpolate(translations[1], 0.5);
+        double done = 2;
+        double total = 2;
+        for (int i = 2; i < translations.length; i++) {
+            total++;
+            interpolated = translations[i].interpolate(interpolated, done / total);
+            done++;
+        }
+
+        return interpolated;
+    }
+
+    /**
+     * Finds a {@link Pose2d} between all of the parameters.
+     * @param poses The translations to analyze
+     */
+    public Pose2d getMean(Pose2d... poses) {
+        if (poses.length == 0) return new Pose2d();
+        if (poses.length == 1) return poses[0];
+
+        Pose2d interpolated = poses[0].interpolate(poses[1], 0.5);
+        double done = 2;
+        double total = 2;
+        for (int i = 2; i < poses.length; i++) {
+            total++;
+            interpolated = poses[i].interpolate(interpolated, done / total);
+            done++;
+        }
+
+        return interpolated;
     }
 }
